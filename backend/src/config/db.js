c380c31f1efe1f4c02b1f3d9659468805950c1b5
Pranav Satish Kharote <pranavkharote2005@gmail.com>
2025-12-06 13:45:25 +0530

    did some deployment changes

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,19 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+// });
+
+
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
-
 pool
   .connect()
   .then(() => console.log("PostgreSQL connected."))
